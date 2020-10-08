10e959f2546c25da630157c9009e123f49d5d139
biancaCastro <biiianca_00@hotmail.com>
2020-10-08 18:50:38 +0200

    added styles in nav and header

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -9,13 +9,16 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href="/">
-        <a>
-          <Logo className={styles.logo} />
-        </a>
-      </Link>
       <nav>
         <ul className={styles.navList}>
+        <div className={styles.logoContainer}>
+        <li>
+            <Link href="/">
+              <a>
+                <Logo className={styles.logo} />
+              </a>
+            </Link>
+          </li>
           <li>
             <Link href="/">
               <a className={styles.link}>Home</a>
@@ -31,19 +34,18 @@ const Footer = () => {
               <a className={styles.link}>Contáctame</a>
             </Link>
           </li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li>
-            <Behance />
-          </li>
-          <li>
-            <Twitter />
-          </li>
-          <li>
-            <Linkedin />
-          </li>
+        </div>
+          <div className={styles.logoContainer}>
+            <li>
+              <Behance />
+            </li>
+            <li>
+              <Twitter />
+            </li>
+            <li>
+              <Linkedin />
+            </li>
+          </div>
         </ul>
       </nav>
     </footer>
